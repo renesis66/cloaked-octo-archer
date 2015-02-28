0e600e8f6a55c9fc00d4abb812afc85673658dca
renesis66 <scott.dierbeck@gmail.com>
2015-02-28 13:47:09 -0700

    Adding tests for Time class

--- src/test/scala/com/typesafe/training/scalatrain/TimeSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/TimeSpec.scala
@@ -8,11 +8,31 @@ import org.scalatest.{ Matchers, WordSpec }
 class TimeSpec extends WordSpec with Matchers {
 
   "Creating time" should {
+    "set default hours and minutes to zero" in {
+      val time = Time()
+      time.hours shouldEqual 0
+      time.minutes shouldEqual 0
+      time.asMinutes shouldEqual 0
+    }
+    "initialize asMinutes properly" in {
+      val time = Time(hours = 1, minutes = 30)
+      time.asMinutes shouldEqual 90
+    }
     "throw IllegalArgumentException" in {
       evaluating(Time(hours = 24)) should produce[Exception]
       evaluating(Time(hours = -1)) should produce[Exception]
       evaluating(Time(minutes = -1)) should produce[Exception]
       evaluating(Time(minutes = 60)) should produce[Exception]
+    }
+  }
+  "Subtracting two Times with minus" should {
+    "return correct difference in minutes" in {
+      Time(1, 30).minus(Time(minutes = 30)) shouldEqual 60
+    }
+  }
+  "Subtracting two Times with -" should {
+    "return correct difference in minutes" in {
+      Time(1, 30) - Time(minutes = 30) shouldEqual 60
     }
   }
 }
